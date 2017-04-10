78934e9d388ea976f889d5fa189c7817fded0f19
Iman Ghafoori <imanghafoori1@gmail.com>
2017-04-10 21:57:05 +0430

    - style fix

--- src/helpers.php
+++ src/helpers.php
@@ -5,6 +5,5 @@ if (!function_exists('render_widget')) {
     function render_widget(BaseWidget $widget, ...$args)
     {
         return app('imanghafoori.widget.renderer')->renderWidget($widget, ...$args);
-
     }
 }
