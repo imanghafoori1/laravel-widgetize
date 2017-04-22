7eb81afc7f59373f297092d8bd52e9b208aaa5e4
imanghafoori <imanghafoori1@gmail.com>
2017-04-22 21:24:54 +0430

    - minor bugfix

--- src/Utils/Normalizers/TemplateNormalizer.php
+++ src/Utils/Normalizers/TemplateNormalizer.php
@@ -12,7 +12,7 @@ class TemplateNormalizer
     public function normalizeTemplateName($widget)
     {
         // class name without namespace.
-        $className = str_replace('App\\Widgets\\', '', class_basename($widget));
+        $className = str_replace('App\\Widgets\\', '', get_class($widget));
 
         // replace slashes with dots
         $className = str_replace(['\\', '/'], '.', $className);
@@ -22,7 +22,7 @@ class TemplateNormalizer
         }
 
         if (! view()->exists($widget->template)) {
-            throw new \InvalidArgumentException("View file [{$className}View] not found by: '".class_basename($widget)." '");
+            throw new \InvalidArgumentException("View file [{$className}View] not found by: '".get_class($widget)." '");
         }
     }
 }
