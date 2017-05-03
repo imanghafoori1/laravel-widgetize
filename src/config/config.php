c9c1930f40075408b10a8f9f5764ccfc8dc85578
imanghafoori <imanghafoori1@gmail.com>
2017-05-03 23:26:42 +0430

    - minor improvements

--- src/config/config.php
+++ src/config/config.php
@@ -6,6 +6,7 @@ return [
     | Cache the HTML
     |--------------------------------------------------------------------------
     | You can Globally turn caching on and off for all widgets from here
+    |
     */
     'enable_cache' => false,
     /*
@@ -15,6 +16,7 @@ return [
     | You Can Specify a fallback value for the cache lifetime of your
     | widgets, so you do not have to define the "public $cacheLifeTime"
     | on each and every widget class in your application.
+    |
     */
     'default_cache_lifetime' => 1, //(minutes)
     /*
@@ -22,6 +24,7 @@ return [
     | Minify Widget HTML
     |--------------------------------------------------------------------------
     | Minify widgets to save both 'cache storage space' and the 'page size'
+    |
     */
     'minify_html' => false,
     /*
@@ -31,6 +34,7 @@ return [
     | It is helpful for you to see information about your widgets in the browser
     | as HTML title attribute.So it is recommended to turn it on for development.
     | * Remember to make it false for in production.
+    |
     */
     'debug_info' => true,
 ];
