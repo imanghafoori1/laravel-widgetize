d821a5447bd6c4445778c09c929f2905bd7e53ed
imanghafoori <imanghafoori1@gmail.com>
2019-01-05 22:13:13 +0330

    remove the view maacro

--- src/RouteMacros.php
+++ src/RouteMacros.php
@@ -8,23 +8,9 @@ class RouteMacros
 {
     public function registerMacros()
     {
-        $this->registerViewMacro();
-
         $this->registerWidget();
 
         $this->registerJsonWidget();
-    }
-
-    private function registerViewMacro()
-    {
-        Route::macro('view', function ($url, $view, $name = null) {
-            return Route::get($url, [
-                'as' => $name,
-                'uses' => function () use ($view) {
-                    return view($view);
-                },
-            ]);
-        });
     }
 
     private function registerWidget()
