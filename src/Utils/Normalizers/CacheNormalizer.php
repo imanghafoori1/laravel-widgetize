b0d309f2a135cbad7b8a530859abdda7106c6e00
imanghafoori <imanghafoori1@gmail.com>
2019-05-18 12:48:39 +0430

    make laravel 5.8 compatible

--- src/Utils/Normalizers/CacheNormalizer.php
+++ src/Utils/Normalizers/CacheNormalizer.php
@@ -15,12 +15,15 @@ class CacheNormalizer implements NormalizerContract
     public function normalize($widget): void
     {
         if (! property_exists($widget, 'cacheLifeTime')) {
-            $widget->cacheLifeTime = (int) (config('widgetize.default_cache_lifetime', 0));
+            $M = (int) (config('widgetize.default_cache_lifetime', 0));
+            $widget->cacheLifeTime = new \DateInterval('PT'.$M.'M');
         }
 
         if ($widget->cacheLifeTime === 'forever' || $widget->cacheLifeTime < 0) {
-            // 20.000 minutes is about 2 weeks which is long enough !
-            $widget->cacheLifeTime = 20000;
+            // 2 weeks which is long enough !
+            $widget->cacheLifeTime = new \DateInterval('P2W');
+        } elseif (is_numeric($widget->cacheLifeTime)) {
+            $widget->cacheLifeTime = new \DateInterval('PT'.(string)$widget->cacheLifeTime.'M');
         }
     }
 }
