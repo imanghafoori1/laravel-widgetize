938c7ae8870abd6b5b901ccdb12bc8dfee095687
iya30n <varmeziyaryasin@gmail.com>
2020-09-13 18:33:07 +0430

    convert tab to spaces

--- src/Utils/SlotRenderer.php
+++ src/Utils/SlotRenderer.php
@@ -4,33 +4,33 @@ namespace Imanghafoori\Widgets\Utils;
 
 trait SlotRenderer
 {
-	protected $slotName;
+    protected $slotName;
 
-	protected $slots = [];
+    protected $slots = [];
 
-	public function startSlot($name)
-	{
-		if (ob_start()) {
-			$this->slotName = $name;
-		}
-	}
+    public function startSlot($name)
+    {
+        if (ob_start()) {
+            $this->slotName = $name;
+        }
+    }
 
-	public function renderSlot($data)
-	{
-		$this->slots[$this->slotName] = $data ?? "";
-	}
+    public function renderSlot($data)
+    {
+        $this->slots[$this->slotName] = $data ?? "";
+    }
 
-	public function hasSlot()
-	{
-		return !empty($this->slots);
-	}
+    public function hasSlot()
+    {
+        return !empty($this->slots);
+    }
 
-	/**
-	 * Assign slots to $_viewData
-	 */
-	private function assignSlots()
-	{
-		if ($this->hasSlot())
-			$this->_viewData = array_merge($this->_viewData, $this->slots);
-	}
+    /**
+     * Assign slots to $_viewData
+     */
+    private function assignSlots()
+    {
+        if ($this->hasSlot())
+            $this->_viewData = array_merge($this->_viewData, $this->slots);
+    }
 }
